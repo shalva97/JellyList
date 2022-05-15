c9815628bda5179949c8a6d55db661da2b4eb0ee
shalva97 <shalva258@gmail.com>
2022-05-15 22:04:31 +0400

    implement dark mode

--- app/src/main/java/com/shalva97/jellylist/MainActivity.kt
+++ app/src/main/java/com/shalva97/jellylist/MainActivity.kt
@@ -3,21 +3,29 @@ package com.shalva97.jellylist
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.shalva97.jellylist.presentation.LoginScreen
-import com.shalva97.jellylist.ui.theme.JellyListTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kiwi.orbit.compose.ui.OrbitTheme
+import kiwi.orbit.compose.ui.controls.Scaffold
+import kiwi.orbit.compose.ui.foundation.darkColors
+import kiwi.orbit.compose.ui.foundation.lightColors
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JellyListTheme {
-                JellyList()
+            OrbitTheme(
+                colors = if (isSystemInDarkTheme()) darkColors() else lightColors(),
+            ) {
+                Scaffold {
+                    JellyList()
+                }
             }
         }
     }
