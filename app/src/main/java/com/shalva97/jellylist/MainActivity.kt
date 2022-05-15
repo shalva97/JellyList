7ed4d4ff04299f81c2c41f4dc897a0172366467d
shalva97 <shalva258@gmail.com>
2022-05-15 23:36:06 +0400

    fix status bars

--- app/src/main/java/com/shalva97/jellylist/MainActivity.kt
+++ app/src/main/java/com/shalva97/jellylist/MainActivity.kt
@@ -3,11 +3,12 @@ package com.shalva97.jellylist
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.shalva97.jellylist.presentation.LoginScreen
 import dagger.hilt.android.AndroidEntryPoint
 import kiwi.orbit.compose.ui.OrbitTheme
@@ -20,9 +21,18 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val isSystemInDarkTheme = true
+            val systemUiController = rememberSystemUiController()
+
             OrbitTheme(
-                colors = if (isSystemInDarkTheme()) darkColors() else lightColors(),
+                colors = if (isSystemInDarkTheme) darkColors() else lightColors(),
             ) {
+                val color = OrbitTheme.colors.surface.subtle
+
+                SideEffect {
+                    systemUiController.setSystemBarsColor(color = color)
+                }
+
                 Scaffold {
                     JellyList()
                 }
