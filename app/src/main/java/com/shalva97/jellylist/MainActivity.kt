007bbeb409344b71aa2fa3ad7a71834b4b55f6af
shalva97 <shalva258@gmail.com>
2022-05-16 22:43:58 +0400

    fix dark theme

--- app/src/main/java/com/shalva97/jellylist/MainActivity.kt
+++ app/src/main/java/com/shalva97/jellylist/MainActivity.kt
@@ -3,8 +3,12 @@ package com.shalva97.jellylist
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.core.view.WindowCompat
 import androidx.navigation.compose.NavHost
@@ -26,7 +30,7 @@ class MainActivity : ComponentActivity() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
         setContent {
-            val isSystemInDarkTheme = true
+            val isSystemInDarkTheme = isSystemInDarkTheme()
             val systemUiController = rememberSystemUiController()
 
             OrbitTheme(
@@ -39,8 +43,10 @@ class MainActivity : ComponentActivity() {
                     )
                 }
 
-                Scaffold() {
-                    JellyList()
+                Scaffold {
+                    Box(modifier = Modifier.systemBarsPadding()) {
+                        JellyList()
+                    }
                 }
             }
         }
