04f33424c2b1bd612b24fa3c0b99e7b62c65b55c
shalva97 <shalva258@gmail.com>
2022-06-05 23:49:56 +0400

    remove hardcoded data

--- app/src/main/java/com/shalva97/jellylist/presentation/login/LoginScreenViewModel.kt
+++ app/src/main/java/com/shalva97/jellylist/presentation/login/LoginScreenViewModel.kt
@@ -9,7 +9,6 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.runningFold
 import kotlinx.coroutines.launch
@@ -20,22 +19,14 @@ class LoginScreenViewModel @Inject constructor(
     private val jellyFinClient: JellyFinRepo,
 ) : ViewModel() {
 
-    val foundServers1 = jellyFinClient.discoverServers()
+    val foundServers = jellyFinClient.discoverServers()
         .runningFold(emptyList<JellyFinServer>()) { accumulator, value -> accumulator + value }
         .flowOn(Dispatchers.IO)
-    val server = mutableStateOf<String>("192.168.")
+    val server = mutableStateOf("192.168.")
 
     val errors = MutableStateFlow<Errors>(Errors.NoErrors)
 
-    val foundServers = flowOf(
-        listOf(
-            JellyFinServer("12312 12312 12 3"),
-            JellyFinServer("12312 12312 12 3"),
-            JellyFinServer("12312 12312 12 3"),
-            JellyFinServer("12312 12312 12 3"),
-        ))
-
-    val loading = MutableStateFlow<Boolean>(false)
+    val loading = MutableStateFlow(false)
 
     fun connectToServer(jellyFinServer: JellyFinServer) =
         viewModelScope.launch(exceptionHandler) {
