cb1cbd829a5b42539049548c44c82274db285a01
shalva97 <shalva258@gmail.com>
2022-07-02 23:05:44 +0400

    update deps and kotlin

--- app/src/main/java/com/shalva97/jellylist/domain/JellyFinServer.kt
+++ app/src/main/java/com/shalva97/jellylist/domain/JellyFinServer.kt
@@ -8,5 +8,5 @@ class JellyFinServer(val address: String) {
         address = serverDiscoveryInfo.address
     )
 
-    constructor(serverDiscoveryInfo: ServerDiscoveryInfo) : this(serverDiscoveryInfo.address!!)
+    constructor(serverDiscoveryInfo: ServerDiscoveryInfo) : this(serverDiscoveryInfo.address)
 }
