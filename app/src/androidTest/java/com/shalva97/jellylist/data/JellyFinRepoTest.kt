9e2ffbb4af2e76cd130e3ee5f054ed04c5dd90d5
shalva97 <shalva258@gmail.com>
2022-07-06 22:56:28 +0400

    convert one file to gradle kts

--- app/src/androidTest/java/com/shalva97/jellylist/data/JellyFinRepoTest.kt
+++ app/src/androidTest/java/com/shalva97/jellylist/data/JellyFinRepoTest.kt
@@ -1,13 +1,8 @@
 package com.shalva97.jellylist.data
 
-import androidx.test.core.app.launchActivity
-import com.shalva97.jellylist.MainActivity
 import dagger.hilt.android.testing.BindValue
 import dagger.hilt.android.testing.HiltAndroidTest
-import io.mockk.every
 import io.mockk.mockk
-import kotlinx.coroutines.flow.flowOf
-import org.jellyfin.sdk.model.api.ServerDiscoveryInfo
 import org.junit.Test
 
 @HiltAndroidTest
@@ -18,10 +13,6 @@ class JellyFinRepoTest {
 
     @Test
     fun bla2h() {
-
-        every { jellyFinClient.discoverServers() } returns
-                flowOf(ServerDiscoveryInfo(address = "blah.com"))
-        val activity = launchActivity<MainActivity>()
 
 
 
--- jellyfin/src/main/kotlin/MyClass.kt
+++ jellyfin/src/main/kotlin/MyClass.kt
@@ -1,6 +1,15 @@
+package com.shalva97.jellyfin
+
 public class MyClass {
     fun test() {
         "asdf".uppercase()
     }
+
+    fun main() {
+
+    }
 }
 
+fun main() {
+
+}
