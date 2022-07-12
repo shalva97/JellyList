193e47a6bd38ff84636937581d2465f83ada550d
shalva97 <shalva258@gmail.com>
2022-07-12 20:52:32 +0400

    add proto data store dependency

--- libraries/recent_servers/build.gradle.kts
+++ libraries/recent_servers/build.gradle.kts
@@ -4,6 +4,11 @@ plugins {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_7
-    targetCompatibility = JavaVersion.VERSION_1_7
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+dependencies {
+    // TODO add rules for proguard
+    implementation("androidx.datastore:datastore-core:1.0.0")
 }
