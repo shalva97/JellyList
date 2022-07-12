8788c83f9da8f65ee67129897925127369e18c70
shalva97 <shalva258@gmail.com>
2022-07-12 20:12:07 +0400

    remove leftover hilt stuff

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-hilt/ hilt in tests compose
 https://osherove.com/blog/2005/4/3/naming-standards-for-unit-tests.html
 https://developer.android.com/kotlin/coroutines/coroutines-best-practices
 https://medium.com/androiddevelopers/coroutines-patterns-for-work-that-shouldnt-be-cancelled-e26c40f142ad
--- app/src/androidTest/java/com/shalva97/jellylist/data/JellyFinRepoTest.kt
+++ app/src/androidTest/java/com/shalva97/jellylist/data/JellyFinRepoTest.kt
@@ -1,19 +1,11 @@
 package com.shalva97.jellylist.data
 
-import dagger.hilt.android.testing.BindValue
-import dagger.hilt.android.testing.HiltAndroidTest
-import io.mockk.mockk
 import org.junit.Test
 
-@HiltAndroidTest
 class JellyFinRepoTest {
-
-    @BindValue
-    val jellyFinClient: JellyFinRepo = mockk()
 
     @Test
     fun bla2h() {
-
 
 
         Thread.sleep(100000)
