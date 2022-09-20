4d2ff04ee1790be7580dac0b47ff1e5a4139c476
shalva97 <shalva258@gmail.com>
2022-09-20 23:36:02 +0400

    complete log out functionality. boiiiiii hehehe

--- app-features/screens/home/src/main/java/com/shalva97/home/Home.kt
+++ app-features/screens/home/src/main/java/com/shalva97/home/Home.kt
@@ -13,10 +13,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
 import kiwi.orbit.compose.icons.Icons
-import kiwi.orbit.compose.ui.controls.ButtonLinkPrimary
-import kiwi.orbit.compose.ui.controls.Icon
-import kiwi.orbit.compose.ui.controls.ListChoice
-import kiwi.orbit.compose.ui.controls.Text
+import kiwi.orbit.compose.ui.controls.*
+import org.jellyfin.sdk.model.api.BaseItemDto
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -27,71 +25,77 @@ fun Home(navigateToLogin: () -> Unit = { }) {
     val homeState by viewModel.state.collectAsState()
 
     when (homeState) {
-        is HomeState.Content -> TODO()
-        HomeState.Loading -> TODO()
-        HomeState.NavigateToLogin -> TODO()
-    }
-
-    if (homeState.navigateToLogin) {
-        navigateToLogin.invoke()
-        viewModel.navigationEventConsumed()
-    } else {
-        Content()
-    }
-
-    @Composable
-    fun Content() {
-        ConstraintLayout(
-            modifier = Modifier
-                .fillMaxSize()
-                .systemBarsPadding()
-        ) {
-            val (logoutButton, content, bottomNavigation) = createRefs()
-
-            ButtonLinkPrimary(onClick = {
-                viewModel.logout()
-            }, modifier = Modifier.constrainAs(logoutButton) {
-                top.linkTo(parent.top)
-                end.linkTo(parent.end)
-            }) {
-                Text(text = "logout")
-            }
-
-            Column(
-                modifier = Modifier
-                    .verticalScroll(rememberScrollState())
-                    .constrainAs(content) {
-                        top.linkTo(logoutButton.bottom)
-                        end.linkTo(parent.end)
-                        start.linkTo(parent.start)
-                        bottom.linkTo(bottomNavigation.top)
-                        height = Dimension.fillToConstraints
-                    }, verticalArrangement = Arrangement.Bottom
-            ) {
-                homeState.movies.forEach {
-                    ListChoice(onClick = { viewModel.openVideoByExternalApp(it) }) {
-                        Text(text = it.name ?: "Unknown")
-                    }
-                }
-            }
-
-            BottomNavigation(
-                Modifier
-                    .fillMaxWidth()
-                    .constrainAs(bottomNavigation) {
-                        bottom.linkTo(parent.bottom)
-                        end.linkTo(parent.end)
-                        start.linkTo(parent.start)
-                    }
-            ) {
-                BottomNavigationItem(selected = true, onClick = { /*TODO*/ }, icon = {
-                    Icon(painter = Icons.List, contentDescription = null)
-                })
-                BottomNavigationItem(selected = false, onClick = { /*TODO*/ }, icon = {
-                    Icon(painter = Icons.Child, contentDescription = null)
-                })
-            }
+        is HomeState.Content -> {
+            Content(
+                homeState as HomeState.Content,
+                viewModel::logout,
+                viewModel::openVideoByExternalApp
+            )
+        }
+        HomeState.Loading ->
+            LinearIndeterminateProgressIndicator(modifier = Modifier.fillMaxWidth())
+        HomeState.NavigateToLogin -> {
+            navigateToLogin.invoke()
+            viewModel.navigationEventConsumed()
         }
     }
+}
 
+@Composable
+fun Content(
+    homeState: HomeState.Content,
+    logout: () -> Unit = {},
+    openVideoByExternalApp: (BaseItemDto) -> Unit = {}
+) {
+    ConstraintLayout(
+        modifier = Modifier
+            .fillMaxSize()
+            .systemBarsPadding()
+    ) {
+        val (logoutButton, content, bottomNavigation) = createRefs()
+
+        ButtonLinkPrimary(onClick = {
+            logout()
+        }, modifier = Modifier.constrainAs(logoutButton) {
+            top.linkTo(parent.top)
+            end.linkTo(parent.end)
+        }) {
+            Text(text = "logout")
+        }
+
+        Column(
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+                .constrainAs(content) {
+                    top.linkTo(logoutButton.bottom)
+                    end.linkTo(parent.end)
+                    start.linkTo(parent.start)
+                    bottom.linkTo(bottomNavigation.top)
+                    height = Dimension.fillToConstraints
+                }, verticalArrangement = Arrangement.Bottom
+        ) {
+            homeState.movies.forEach {
+                ListChoice(onClick = { openVideoByExternalApp(it) }) {
+                    Text(text = it.name ?: "Unknown")
+                }
+            }
+        }
+
+        BottomNavigation(
+            Modifier
+                .fillMaxWidth()
+                .constrainAs(bottomNavigation) {
+                    bottom.linkTo(parent.bottom)
+                    end.linkTo(parent.end)
+                    start.linkTo(parent.start)
+                }
+        ) {
+            BottomNavigationItem(selected = true, onClick = { /*TODO*/ }, icon = {
+                Icon(painter = Icons.List, contentDescription = null)
+            })
+            BottomNavigationItem(selected = false, onClick = { /*TODO*/ }, icon = {
+                Icon(painter = Icons.Child, contentDescription = null)
+            })
+        }
+    }
 }
